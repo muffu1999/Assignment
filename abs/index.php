6c3174057fcad6d38acbabfd41023ebfb8995b2a
muffu1999 <67001550+muffu1999@users.noreply.github.com>
2020-10-15 18:55:43 +0530

    Add files via upload

---
+++ abs/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mobile</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+    
+</head>
+<body>
+       <div class="col-sm-12 mydiv1" style="margin-bottom:5px">
+           <div style="float:right">
+               <i class='fas fa-square-full' style="color:silver"></i>
+               <i class='fas fa-circle' style="color:silver"></i>
+               <i class='fas fa-caret-down' style="color:silver"></i>
+            </div>
+            
+       </div>
+       <div class="col-sm-12 mydiv2">
+               <i class='fas fa-user-circle' style="font-size:20px;color:red"></i>
+               <i class='fas fa-sign-out-alt' style="float:right;color:red"></i>
+                <span style="font-weight:bold">Mahalaxmi car wash</span><br>
+               <span style="font-weight:500;margin-left:24px">Munna bhai MBBS</span>
+               
+        </div>
+           <div class="col-sm-12 mydiv3">
+             <div class="row">
+              <div class="col-sm-6 riders">
+                  <span>Riders</span>
+              </div>
+              <div class="col-sm-6 new">
+                  <span>New</span>
+              </div>
+              
+              </div>
+              <div class="col-sm-12 mydiv4">
+                 <div class="active">Active</div>
+                 <div style="margin-top:5px">
+                 <i class='fas fa-user-alt'></i><span style="font-weight:bold;margin-left:8px">Raju Shrivatsav</span><br>
+                 <i class='fas fa-location-arrow'></i><span style="font-weight:bold;margin-left:8px">Near Venkeys,Parihar Chowk,Aundh</span><br>
+                 <i class='fas fa-phone-alt'><span style="font-weight:bold;margin-left:8px">+91 12 12 123 123</span></i>
+                 </div>
+                  
+              </div>
+              <div class="col-sm-12 mydiv4">
+                 <div class="dormant">Dormant</div>
+                 <div style="margin-top:5px">
+                 <i class='fas fa-user-alt'></i><span style="font-weight:bold;margin-left:8px">Raju Shrivatsav</span><br>
+                 <i class='fas fa-location-arrow'></i><span style="font-weight:bold;margin-left:8px">Near Venkeys,Parihar Chowk,Aundh</span><br>
+                 <i class='fas fa-phone-alt'><span style="font-weight:bold;margin-left:8px">+91 12 12 123 123</span></i>
+                 </div>
+                  
+              </div>
+              <div class="col-sm-12 mydiv4">
+                 <div class="active">Active</div>
+                 <div style="margin-top:5px">
+                 <i class='fas fa-user-alt'></i><span style="font-weight:bold;margin-left:8px">Raju Shrivatsav</span><br>
+                 <i class='fas fa-location-arrow'></i><span style="font-weight:bold;margin-left:8px">Near Venkeys,Parihar Chowk,Aundh</span><br>
+                 <i class='fas fa-phone-alt'><span style="font-weight:bold;margin-left:8px">+91 12 12 123 123</span></i>
+                 </div>
+                  
+              </div>
+               
+           </div>
+           <div class="col-sm-12">
+              <i class='fas fa-bars'></i>
+               <div style="float:right">
+                  <i class="fas fa-file" style="margin-right:10px"></i>
+                   <i class='fas fa-bell'></i>
+               </div>
+           </div>
+            
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <Script src="js/a076d05399.js"></Script>
+</body>
+</html>
